519598044672e100a5238ead90d4635f48b93e61
Marius <marius@abc.x>
2018-12-04 15:57:08 +0200

    bug fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Response, Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  _albumUrl: string = "../assets/album.json";
+  constructor(private _http: Http) { }
 
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(respnse => respnse.json());
+  }
 }
